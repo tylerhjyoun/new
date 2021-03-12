ac12161080172f97e7681352233a1da0a3861f59
vanisharmag <67726421+vanisharmag@users.noreply.github.com>
2021-03-11 21:16:54 -0800

    Update showgroups.component.js

--- mern-egg/src/components/showgroups.component.js
+++ mern-egg/src/components/showgroups.component.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+// import Timer from './timer.component'
 import axios from 'axios'
 
+// const Group = props => (
+//     <tr>
+//         <td>{props.group.groupName}</td>
+//         <Link to={`/users/${props.group.groupMembers[0]}`}>       
+//             <td>{props.group.groupMembers}</td>
+//         </Link>
+//         {/* <td>...members...</td> */}
+//         <td>{props.group.groupCount}</td>
+//         <td>
+//             <button class = "RemoveButton" 
+//             href="#" onClick={() => { props.deleteGroup(props.group._id) }}>delete</button>
+//         </td>
+//     </tr>
+// );
 const Group = props => (
     <tr>
         <td>{props.group.groupName}</td>
-        <Link to={`/users/${props.group.groupMembers[0]}`}>       
-            <td>{props.group.groupMembers}</td>
-        </Link>
-        {/* <td>...members...</td> */}
+        <td>{props.group.groupMembers.map(m => m['username'] + ' ')}</td>
         <td>{props.group.groupCount}</td>
+        {/* <td><Timer endtime={props.group.groupEndtime 
+            idk how to make the endtime field 
+            if each group has events for all of the members then would need to change how we implement create event
+                - i tried adding a change user function so that it could be an array but it didnt work yet
+        } /></td>  */}
         <td>
-            <a href="#" onClick={() => { props.deleteGroup(props.group._id) }}>delete</a>
+            <button class = "RemoveButton" 
+            href="#" onClick={() => { props.deleteGroup(props.group._id) }}>delete</button>
         </td>
+        {/* <td>
+            <button class = "RemoveButton" 
+            href="#" onClick={() => { props.addEvent(props.group._id) }}>add event</button>
+        </td> */}
     </tr>
 );
 
@@ -35,32 +57,65 @@ export default class ShowGroups extends Component {
       })    
     }
 
-    componentDidMount() { 
-        axios.get(`http://localhost:5000/groups`)
-            .then(res => {
-                console.log(res.data)
-                const groupsData = res.data
-                var final = []
+    // <Link to='/home/event'> Add Event </Link> 
+    // how the add event works is a link to the add event page 
 
-                for (var i = 0; i < groupsData.length; i++) { 
-                    var curr = groupsData[i]
-                    /* bro */
-                    const usertoken = { usertoken: localStorage.getItem('data') }
-                    if (curr["groupMembers"] === usertoken) { 
-                        final.push(curr)
-                    } 
+    // componentDidMount() { 
+    //     axios.get(`http://localhost:5000/groups`)
+    //         .then(res => {
+    //             console.log(res.data)
+    //             const groupsData = res.data
+    //             var final = []
+
+    //             for (var i = 0; i < groupsData.length; i++) { 
+    //                 var curr = groupsData[i]
+    //                 /* bro */
+    //                 const usertoken = { usertoken: localStorage.getItem('data') }
+    //                 if (curr["groupMembers"] === usertoken) { 
+    //                     final.push(curr)
+    //                 } 
         
-                }
+    //             }
 
-                this.setState({
-                    groups: final,
-                });
+    //             this.setState({
+    //                 groups: final,
+    //             });
                 
+    //         })
+    //         .catch((error) => {
+    //             console.log(error);
+    //           })
+
+    // }
+    componentDidMount() {
+        const token = { token: localStorage.getItem('data') } // determine user that is logged in and set state to its id
+        axios.post(`http://localhost:5000/login/token`, token)
+            .then((res) => {
+                this.setState({ id: res.data.id.id })
+                axios.get(`http://localhost:5000/groups`)
+                    .then(response => {
+                        const all = response.data; 
+                        let usergroups = [];
+                        for(let i = 0; i < all.length; i++){
+                            const currentgroup = all[i]
+                            for(let j = 0; j < currentgroup.groupMembers.length; j++){
+                                const currentgroupmembersIds = currentgroup.groupMembers[j]['id']
+                                if(currentgroupmembersIds === this.state.id){
+                                    usergroups.push(currentgroup)
+                                }
+                            }
+                        }
+                        this.setState({
+                            groups: usergroups,
+                        })
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    })
             })
             .catch((error) => {
                 console.log(error);
-              })
-
+                        })
     }
 
     groupList(){
@@ -71,10 +126,7 @@ export default class ShowGroups extends Component {
     render() {
         return (
             <div>
-               <h2> Groups </h2>
-               <form action="/home/group">
-                    <input className = "AddEvent" type="submit" value="Add Group" />
-                </form>
+               <h2> Logged Groups </h2>
                <Link to='/home/group'> Add Group </Link>
             <table>
             <thead>
@@ -82,6 +134,7 @@ export default class ShowGroups extends Component {
                     <th>Group Name</th>
                     <th>Members</th>
                     <th>Number of Members</th>
+                    {/* <th>Timer</th> */}
                 </tr>
             </thead>
                 <tbody>
