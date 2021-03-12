8613c5b3ef4c70cf0cac291db870a36594d5941c
vanisharmag <67726421+vanisharmag@users.noreply.github.com>
2021-03-11 21:41:22 -0800

    Update myevents.component.js

--- mern-egg/src/components/myevents.component.js
+++ mern-egg/src/components/myevents.component.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios'
-
+import Timer from './timer.component'
 import moment from 'moment'
 
 import '../Events.css';
@@ -12,6 +12,7 @@ const Event = props => (
         <td>{props.event.eventname}</td>
         <td>{props.event.description}</td>
         <td><b>{moment(props.event.starttime).format("ddd, MMM DD HH:mm a")}</b> until <b>{moment(props.event.endtime).format("ddd, MMM DD HH:mm a")}</b></td>
+        <td><Timer endtime={props.event.endtime} /></td> 
         <td>
             <button class = "RemoveButton" 
             href="#" onClick={() => { props.deleteEvent(props.event._id) }}>Remove</button>
@@ -71,11 +72,6 @@ export default class MyEvents extends Component {
                         })
     }
 
-    userTimer(timer) {
-        axios.post('http://localhost:5000/users/timer/' + this.state.id, { timer })
-            .then(response => { console.log(response.data) });
-    }
-
     eventList() {
         this.state.events.sort(function(a, b) {
             const aval = Math.floor((new Date(a.endtime).getTime() + new Date(a.endtime).getDate() - new Date().getTime() - new Date().getDate())/ (1000));
@@ -88,10 +84,7 @@ export default class MyEvents extends Component {
         return <Event event={currentevent} deleteEvent={this.deleteEvent} key={currentevent._id} />
         })
     }
-   /* i couldnt figure out how to delete old events 
-        - i tried to setState to the filtered list but it gets called too many times so it thinks theres an infinite loop
-        - i tried writing a clean function and then using set interval to call it but i couldnt get it to work since its async and tried using th efunction before there was any data idk 
-        with an old list the user can delete the events later */
+   
     oldList() { 
         this.state.events.sort(function(a, b) {
             const aval = Math.floor((new Date(a.endtime).getTime() + new Date(a.endtime).getDate() - new Date().getTime() - new Date().getDate())/ (1000));
@@ -108,32 +101,30 @@ export default class MyEvents extends Component {
     render() {
         return (
             <div>
-               <h2>Events </h2> 
-               <form action="/home/event">
-                    <input className = "AddEvent" type="submit" value="Add Event" />
-                </form>
-               <Link to='/home/event'> Add Event </Link><br/><br/>
-            <h3> Upcoming Events: </h3>
+               <h2>Upcoming Events </h2> 
+               <Link to='/home/event'> Add Event </Link>
+            <input class="form-control" id="myInput" type="text" placeholder="Search.."/>
             <table className="table">
             <thead className="thead-custom">
                 <tr>
                     <th>Event Name</th>
                     <th>Description</th>
                     <th>Duration</th>
+                    <th>Timer</th>
                 </tr>
             </thead>
                 <tbody>
                 { this.eventList() }
                 </tbody>
             </table>
-            <h3> Past Events: </h3>
+            <h2>Past Events </h2> 
             <table className="table">
-            <thead className="thead-custom">
+            <thead className="thead-light">
                 <tr>
                     <th>Event Name</th>
                     <th>Description</th>
                     <th>Duration</th>
-                    <th></th>
+                    <th>Timer</th>
                 </tr>
             </thead>
                 <tbody>
